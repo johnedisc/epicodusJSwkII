3a627d720a54ba07c457ce18f5b8082ec85887b9
johnedisc <christopher.johnedis@gmail.com>
2023-01-09 23:46:48 -0800

    set up blank html for js

---
+++ practice1.js
@@ -0,0 +1,22 @@
+function sayHi() {
+	return 'hello from epicodus';
+}
+
+console.log(sayHi());
+
+let number1 = 77 + 99;
+
+console.log(number1);
+
+
+let favNum = 14;
+let stringNum = String(favNum);
+console.log(typeof stringNum)
+console.log(stringNum);
+
+
+let word = "foo";
+
+word += "bar";
+
+console.log(word);
